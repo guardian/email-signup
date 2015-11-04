72474b02686cde89e422b9e348fc67e5027e63a1
Francis Carr <fran@fcarr.com>
2015-11-04 15:56:43 +0000

    Split out tasks in gulp to clear single tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var fs = require('vinyl-fs');
 var gulp   = require('gulp');
 var lambda = require('gulp-awslambda');
 var zip    = require('gulp-zip');
@@ -10,44 +11,75 @@ var lambdaOptions = {
     region: 'eu-west-1'
 };
 
-gulp.task('updateEmailIngest', function() {
+//Email Ingestion
+gulp.task('buildEmailIngestHandler', function() {
+    var Config = require('./email-signup-config');
+    return gulp.src([
+            'email-signup-config.js',
+            'emailingest.js',
+            'node_modules/validator/*'])
+        .pipe(zip('dist/email-ingest-handler.zip'))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('uploadEmailIngestHandler', function() {
+    return fs.src('dist/email-ingest-handler.zip')
+        .pipe(s3.dest({
+            Bucket: 'aws-frontend-artifacts',
+            Key: 'lambda'
+        }));
+});
+
+gulp.task('updateEmailIngestHandler', function() {
     var Config = require('./email-signup-config');
 
     var emailIngestHandlerConfig = {
-      FunctionName: Config.Lambda.emailIngestHandlerName
+        FunctionName: Config.Lambda.emailIngestHandlerName
     };
 
-    return gulp.src(['email-signup-config.js', 'emailingest.js', 'node_modules/validator/*'])
-        .pipe(zip('dist/email-ingest-handler.zip'))
+    return gulp.src('dist/email-ingest-handler.zip')
         .pipe(lambda(emailIngestHandlerConfig, lambdaOptions))
+});
+
+//Email Subscribe
+gulp.task('buildSubscribeHandler', function() {
+    var Config = require('./email-signup-config');
+    return gulp.src([
+            'subscribehandler.js',
+            'email-signup-config.js',
+            'node_modules/fuel-soap**/**/*',
+            'node_modules/bluebird**/**/*'])
+        .pipe(zip('dist/subscribe-handler.zip'))
         .pipe(gulp.dest('.'));
+});
+
+gulp.task('uploadSubscribeHandler', function() {
+    return fs.src('dist/subscribe-handler.zip')
+        .pipe(s3.dest({
+            Bucket: 'aws-frontend-artifacts',
+            Key: 'lambda'
+        }));
 });
 
 gulp.task('updateSubscribeHandler', function() {
     var Config = require('./email-signup-config');
-
     var subscribeHandlerConfig = {
-      FunctionName: Config.Lambda.exactTargetHandlerName,
-      Timeout: 15
+        FunctionName: Config.Lambda.exactTargetHandlerName,
+        Timeout: 15
     };
 
-    return gulp.src([
-      'subscribehandler.js',
-      'email-signup-config.js',
-      'node_modules/fuel-soap**/**/*',
-      'node_modules/bluebird**/**/*'])
-        .pipe(zip('dist/subscribe-handler.zip'))
+    return gulp.src('dist/subscribe-handler.zip')
         .pipe(lambda(subscribeHandlerConfig, lambdaOptions))
-        .pipe(gulp.dest('.'));
 });
 
 //Cloudformation Tasks
 gulp.task('buildCloudformation', function() {
-  return gulp.src('./cloudformation/*.yml')
-    .pipe(yaml({ schema: 'DEFAULT_SAFE_SCHEMA' }))
-    .pipe(gulp.dest('./dist'));
+    return gulp.src('./cloudformation/*.yml')
+        .pipe(yaml({ schema: 'DEFAULT_SAFE_SCHEMA' }))
+        .pipe(gulp.dest('./dist'));
 });
 
+//Credentials
 gulp.task('downloadCredentials', function() {
     return s3.src('s3://aws-frontend-artifacts/lambda/email-signup-config.js', { buffer: false })
         .pipe(flatten())
