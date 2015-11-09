cc02e6ed6cfcf4ba5e0664e00e2528d9fc907792
Francis Carr <fran@fcarr.com>
2015-11-09 16:31:52 +0000

    Fix fuel soap types to work better

--- manual-typings/fuel-soap/fuel-soap.d.ts
+++ manual-typings/fuel-soap/fuel-soap.d.ts
@@ -1,54 +1,53 @@
 /// <reference path="../tsd.d.ts" />
 
-declare class FuelSoap {
-    constructor(config: FuelSoapCredentials);
-    create(
-        soapType: string,
-        triggeredSend: TriggeredSend,
-        createOption: any,
-        callback: (error: any, response: any) => any
-    ): any;
-}
-
-interface ExtraAttribute {
+declare interface ExtraAttribute {
     Name: string;
     Value: string;
 }
 
-interface ListSubscriber {
+declare interface ListSubscriber {
     ID: number;
     Status: string;
 }
 
-interface Subscriber {
+declare interface Subscriber {
     EmailAddress: string;
     SubscriberKey: string;
     Lists?: Array<ListSubscriber>;
     Attributes?: Array<ExtraAttribute>;
 }
 
-interface TriggeredSendDefinition {
+declare interface TriggeredSendDefinition {
     CustomerKey: string;
 }
 
-interface TriggeredSend {
+declare interface TriggeredSend {
     TriggeredSendDefinition: TriggeredSendDefinition;
     Subscribers: Array<Subscriber>;
 }
 
-interface SaveOptionProperty {
+declare interface SaveOptionProperty {
     PropertyName: string;
     SaveAction: string;
 }
 
-interface SaveOption {
+declare interface SaveOption {
     SaveOption: SaveOptionProperty;
 }
 
-interface CreateOption {
+declare interface CreateOption {
     SaveOptions: Array<SaveOption>;
 }
 
+declare class FuelSoap {
+    constructor(config: FuelSoapCredentials);
+
+    create(soapType:string,
+           triggeredSend:TriggeredSend,
+           createOption:any,
+           callback:(error:any, response:any) => any);
+}
+
 declare module 'fuel-soap' {
-    export default FuelSoap;
+    export = FuelSoap
 }
